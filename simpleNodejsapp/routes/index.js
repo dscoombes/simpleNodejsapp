f39a0b269dbdd690b7c1703708d5d4a113a06b2e
David Coombes <davidc@contentmaster.com>
2019-01-25 10:29:39 +0000

    Initial upload

---
+++ simpleNodejsapp/routes/index.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Contoso Fashions' });
+});
+router.get('/index', function (req, res) {
+    res.render('index', { title: 'Contoso Fashions' });
+});
+router.get('/about', function (req, res) {
+    res.render('about', { title: 'About Us' });
+});
+router.get('/contact', function (req, res) {
+    res.render('contact', { title: 'Contact Us' });
+});
+
+module.exports = router;
